97dd9e10d2b098a761c8b2152f3826fa08980085
ChunJie Tan <chunjietan@ChunJies-MacBook-Air.local>
2026-06-28 01:10:13 +0800

    update java

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Teating purpose");
+
+        System.out.println("Teating purpose");
+        System.out.println("Teating purpose");
+        System.out.println("Teating purpose");
     }
 }
